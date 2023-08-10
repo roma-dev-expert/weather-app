bd679e9e8a512bad21b08c876ec89adf2138fe3f
roma-dev-expert <roma.k.devexpert@gmail.com>
2023-08-10 13:24:03 +0300

    fix: change http to https in geoCoding api

--- src/api/geoCodingApi.js
+++ src/api/geoCodingApi.js
@@ -2,7 +2,7 @@ const fetchCityCoordinates = async (region) => {
 	const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 	try {
-	  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${region}&appid=${apiKey}`);
+	  const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${region}&appid=${apiKey}`);
 	  const data = await response.json();
 	  return data;
 	} catch (error) {
