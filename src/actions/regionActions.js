7b9b3a17ad44df55329b9ed5fcfc1d1d65de250b
roma-dev-expert <roma.k.devexpert@gmail.com>
2025-01-07 15:23:00 +0300

    fix: add regionAction types

--- src/actions/regionActions.js
+++ src/actions/regionActions.js
@@ -1,6 +1,8 @@
+import { SET_SELECTED_REGION } from '../store/regionActionTypes';
+
 export const setSelectedRegion = (regionName) => {
 	return {
-	  type: 'SET_SELECTED_REGION',
+	  type: SET_SELECTED_REGION,
 	  payload: regionName,
 	};
   };
